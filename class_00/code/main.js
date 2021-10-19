2f9bc91aa3b0402a6fe9af42f26edeb535ace04f
ga-wolf <jack.jeffress@ga.co>
2021-10-19 21:32:06 +1100

    Added code from class 00

---
+++ class_00/code/main.js
@@ -0,0 +1,25 @@
+console.log(10.1);
+
+console.log(-15);
+
+// Addition
+10 + 2;
+
+// Subtraction
+5 - 3;
+
+// Multiplication
+5 * 10;
+
+// Division
+6 / 2;
+
+// Booleans - Logical Type
+console.log(true);
+console.log(false);
+
+// Strings - Pieces of text
+console.log("Hello");
+
+// undefined - There is no value here
+console.log(undefined);
